1c2e7d4a2fb04ea9b2a30577463000401e1d17b7
Tom Khrustalev <53542370+tomkhr@users.noreply.github.com>
2024-12-10 16:06:20 -0700

    NOT SUBMITTING THIS

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello!')
+// let age = "";
+// let liveAlone = "";
+// let family = "";
+// let cars = "";
+// let pets = "";
+// let outgoing = "";
+// let income = "";
+
+const allButtons = document.querySelectorAll(".btnA")
+
+allButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        allButtons.forEach(function(btnA) {
+            btnA.classList.remove("selected");
+        });
+
+        this.classList.add("selected");
+    });
+});
+
+
+document.getElementById("all").classList.add("selected");
+
+
